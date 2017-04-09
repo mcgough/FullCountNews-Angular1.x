b28f4f9d9b4dfb5e688e3ab22bc9958da05f3612
Dan McGough <mcgough.dan@gmail.com>
2017-04-08 17:08:32 -0700

    player stats updated on show

--- assets/js/app/controllers/players/UserPlayerShowCtrl.js
+++ assets/js/app/controllers/players/UserPlayerShowCtrl.js
@@ -10,24 +10,32 @@ PlayerTracker.controller('UserPlayerShowCtrl',['$scope','$routeParams','UserServ
   });
 
   var Headline = $resource('/api/headline/:id');
-
   $http.get('/api/playerdb/' + $routeParams.id).success(function(data){
+    $('.loading').addClass('active');
     var player = data;
+    $http.get('/api/playerdb/getPlayerStats',
+      {params:{
+        name: player.name,
+        url: player.url
+      }})
+    .success(function(response) {
+      var player = response;
+      if (player.position.indexOf('P') > -1) {
+        player.pitcher = true;
+      } else {
+        player.pitcher = false;
+      }
 
-    if (player.position.indexOf('P') > -1) {
-      player.pitcher = true;
-    } else {
-      player.pitcher = false;
-    }
-
-    $scope.player = player;
-
-    $http.get('/api/headline/getUserPlayerSpecifcNews',{params:{
-      player: player.name
-    }})
-    .success(function(data) {
-      $scope.news = data;
+      $scope.player = player;
+      $('.loading').removeClass('active');
+      $http.get('/api/headline/getUserPlayerSpecifcNews',{params:{
+        player: player.name
+      }})
+      .success(function(data) {
+        $scope.news = data;
+      });
     });
+
   });
 
   // $http.get('/api/headline/getUserPlayerSpecifcNews/' + $routeParams.id).success(function(data){
